29debbe4db2559a5dea2e9af9628530af74aca90
kartik kumar <gtgkartik@gmail.com>
2023-10-05 16:29:28 +0530

    Added new folders

--- vitap-webapp-frontend/src/app/page.js
+++ vitap-webapp-frontend/src/app/page.js
@@ -1,32 +1,29 @@
-"use client"
-import React, { useState, useEffect } from 'react';
+"use client";
+import React, { useState, useEffect } from "react";
 import NavbarBottom from "@/components/HomePage Components/Navbar/NavbarBottom";
 import NavbarUpper from "@/components/HomePage Components/Navbar/NavbarUpper";
-import axios from 'axios';
+import axios from "axios";
 import Image from "next/image";
-import headerimage from "../assets/images/Homepage Images/HeaderImage.png";
+import headerimage from "../assets/images/Homepage Images/HeaderImage2.png";
 
 export default function Home() {
-  const [heroSections, setHeroSections] = useState([]);
-
   useEffect(() => {
     // Fetch data from the API
-    axios.get('http://localhost:1337/api/home-page-hero-sections')
-      .then(response => {
-        console.log(response.data)
+    axios
+      .get("http://localhost:1337/api/home-page-hero-sections")
+      .then((response) => {
+        console.log(response.data);
       })
-      .catch(error => console.error('Error fetching data:', error));
+      .catch((error) => console.error("Error fetching data:", error));
   }, []);
 
   return (
     <>
       <NavbarUpper />
-      <NavbarBottom/>
+      <NavbarBottom />
 
-      <div className="w-full h-[960px] object-cover">
-        {/* Display the first item's createdAt attribute */}
-
-        <Image src={headerimage} alt="university image" className="w-full h-full" />
+      <div className="w-full h-auto object-cover">
+        <Image src={headerimage} alt="university image" className="w-full" />
       </div>
     </>
   );
--- vitap-webapp-frontend/src/components/HomePage Components/Navbar/NavbarBottom.js
+++ vitap-webapp-frontend/src/components/HomePage Components/Navbar/NavbarBottom.js
@@ -1,8 +1,20 @@
+import logo from "../../../assets/images/logos/vitaplogo.png"
+
 const NavbarBottom = () => {
     return (
       <>
       <div className="sticky top-0">
-        <div className="bg-gray-900 z-20 shadow-sm w-full h-[85px] bg-opacity-50 absolute "></div>
+        <div className="bg-gray-900 z-20 shadow-sm w-full h-[85px] bg-opacity-50 absolute flex ">
+          <ul className="flex">
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+
+          </ul>
+        </div>
       </div>
       </>
     );
