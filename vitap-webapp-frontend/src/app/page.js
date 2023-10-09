2a3eb563482c84a7d9059fb0759b33e58b5e1421
Mohith Bayya <bayyamohith32@gmail.com>
2023-10-09 08:42:28 +0530

    undo page section

--- vitap-webapp-frontend/src/app/page.js
+++ vitap-webapp-frontend/src/app/page.js
@@ -20,9 +20,9 @@ export default function Home() {
 
   return (
     <>
-      {/* <NavbarUpper />
+      <NavbarUpper />
       <NavbarBottom />
-      <HeroSection/> */}
+      <HeroSection/>
       <ResearchInnovation />
     </>
   );
