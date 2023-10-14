076040126e151c46e85e87008a84f36eca88dd80
Aditi Shaw <89793631+aditishaw2417@users.noreply.github.com>
2023-10-14 12:53:10 +0530

    fixed content in one page

--- vitap-webapp-frontend/src/components/HomePage Components/Life at VIT-AP Section/LifeAtVITAP.js
+++ vitap-webapp-frontend/src/components/HomePage Components/Life at VIT-AP Section/LifeAtVITAP.js
@@ -13,67 +13,71 @@ import exp9 from "../assets/explorePhotos/explore9.png";
 const Life = () => {
   return (
     <>
-      <div className="flex flex-col h-[960px] bg-[#E8E8E8]">
-        <Image src={exp1} width={292} height={281} className="absolute" />
+      <div className="flex flex-col h-screen bg-[#E8E8E8]">
+        <Image
+          src={exp1}
+          width={250}
+          height={200}
+          className="absolute top-3 left-[50px]"
+        />
         <Image
           src={exp2}
           width={184}
           height={147}
-          className="absolute top-0 left-[420px]"
+          className="absolute top-0 left-[490px]"
         />
         <Image
           src={exp3}
-          width={288}
-          height={231}
-          className="absolute top-0 left-[732px]"
+          width={250}
+          height={200}
+          className="absolute top-0 left-[820px]"
         />
         <Image
           src={exp4}
-          width={293}
-          height={258}
-          className="absolute top-[72px] right-0"
+          width={250}
+          height={180}
+          className="absolute top-[40px] right-[30px]"
         />
         <Image
           src={exp5}
-          width={396}
-          height={247}
-          className="absolute left-0 top-[317px]"
+          width={320}
+          height={220}
+          className="absolute left-[15px] top-[240px]"
         />
         <Image
           src={exp6}
           width={316}
-          height={210}
-          className="absolute top-[419px] right-0"
+          height={200}
+          className="absolute top-[300px] right-[20px]"
         />
         <Image
           src={exp7}
-          width={292}
-          height={212}
-          className="absolute left-0 top-[645px]"
+          width={272}
+          height={200}
+          className="absolute left-[70px] top-[500px]"
         />
         <Image
           src={exp8}
-          width={396}
-          height={247}
-          className="absolute bottom-0 right-[522px] top-[713px]"
+          width={280}
+          height={202}
+          className="absolute bottom-0 right-[590px] top-[534px]"
         />
         <Image
           src={exp9}
-          width={396}
-          height={247}
-          className="absolute bottom-0 right-0 top-[713px]"
+          width={280}
+          height={202}
+          className="absolute bottom-0 right-[40px] top-[534px]"
         />
-        <h4 className="mt-[349px] text-[27px] text-center items-center relative text-[#5C0E14] font-Emilo font-bold">
+        <h4 className="mt-[210px] text-[27px] text-center items-center relative text-[#5C0E14] font-Emilo font-bold">
           <i>Explore our campus</i>
         </h4>
-        <h1 className="text-[90px] mt-2 text-center items-center relative font-Emilo font-bold">
+        <h1 className="text-[90px] mt-[5px] text-center items-center relative font-Emilo font-bold">
           Life @ VIT-AP
         </h1>
         <p className="w-[616px] h-[54px] text-center ml-[30%] mt-3">
-          Lorem ipsum dolor sit amet consectetur.Lorem ipsum dolor sit amet
-          consectetur amet consectetur.
+          Welcome to VIT-AP!
         </p>
-        <button className="mt-3 ml-[44%] bg-[#5C0E14] items-center text-[#FDFEFF] w-[185px] h-[64px] rounded-full hover:bg-[#7A1820]">
+        <button className="ml-[44%] bg-[#5C0E14] items-center text-[#FDFEFF] w-[185px] h-[64px] rounded-full hover:bg-[#7A1820]">
           EXPLORE
         </button>
       </div>
