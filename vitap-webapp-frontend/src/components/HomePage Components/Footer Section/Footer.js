1a820d50341b9460e5c343ebea14ef2581250835
Anshul Ojha <128066014+tensorflow4545@users.noreply.github.com>
2023-10-14 12:32:24 +0530

    Update Footer.js

--- vitap-webapp-frontend/src/components/HomePage Components/Footer Section/Footer.js
+++ vitap-webapp-frontend/src/components/HomePage Components/Footer Section/Footer.js
@@ -6,9 +6,9 @@ export default function Footer(){
     
     return(
         <>  
-
-        <div class="w-full">
-          <div class="footer">
+           {/* DESKTOP VIEW */}
+           <div class="sm:grid hidden w-full">
+          <div class="footer ">
              
              <div class="flex flex-row">
              <div class="footer1 basis-3/4">
@@ -25,7 +25,7 @@ export default function Footer(){
                 </ul>
                 </div>
                 <div class="part2">
-                <p class="heading">Quick Links</p>
+                <p class="heading">VIT Connect</p>
                 <ul class="list">
                     <li class="listitem">V-TOP Login</li>
                     <li class="listitem">V-TOP Parent Login </li>   
@@ -35,7 +35,7 @@ export default function Footer(){
                 </ul>
                 </div>
                 <div class="part3">
-                <p class="heading">Quick Links</p>
+                <p class="heading">Ranking & Accereditation</p>
                 <ul class="list">
                     <li class="listitem">Rankings</li>
                     <li class="listitem">Accreditation </li>   
@@ -49,7 +49,7 @@ export default function Footer(){
                 </div>
                 <div class="flex flex-row">
                <div class="part1">
-                <p class="heading">Quick Links</p>
+                <p class="heading">Research</p>
                 <ul class="list">
                     <li class="listitem">Centres</li>
                     <li class="listitem">Google Scholars</li>   
@@ -60,7 +60,7 @@ export default function Footer(){
                 </ul>
                 </div>
                 <div class="part2">
-                <p class="heading">Quick Links</p>
+                <p class="heading">Programs</p>
                 <ul class="list">
                     <li class="listitem">Undergraduate</li>
                     <li class="listitem">Postgraduate</li>   
@@ -70,7 +70,7 @@ export default function Footer(){
                 </ul>
                 </div>
                 <div class="part3">
-                <p class="heading">Quick Links</p>
+                <p class="heading">Information</p>
                 <ul class="list">
                     <li class="listitem">Public Note</li>
                     <li class="listitem">Academic Calendar</li>   
@@ -83,7 +83,7 @@ export default function Footer(){
                
                 </div>
              </div>
-             <div class="footer2 basis-1/4">
+             <div class="footer2 w-full md:w-full basis-1/4">
                   <div>
                   <iframe class="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3825.6106982247265!2d80.49881150582188!3d16.495238672470556!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a35f27d40f21c55%3A0x1490eacd54859850!2sVIT-AP%20University!5e0!3m2!1sen!2sin!4v1696503206483!5m2!1sen!2sin"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                   </div>
@@ -106,7 +106,7 @@ export default function Footer(){
              </div>
              
 
-             <div class="credit">
+             <div class="credit lg:w-full md:w-max ">
              <div class="flex flex-row">
              <div class="basis">
              <div class="pt-4 pb-5"><Image class="unilogo " src="/unilogo.png" width="240" height="100"></Image></div>
@@ -142,7 +142,100 @@ export default function Footer(){
                     </div>
                 </div>
                 </div>
-              </div>
+                </div>
+
+                {/* MOBILE VIEW */}
+                <div class="sm:hidden flex flex-column">
+
+                    <div class="mcredit w-max h-max">
+                    {/* <div><Image class="unilogo " src="/unilogo.png" width="240" height="100"></Image></div> */}
+                    <div class="icons pl-14 pt-8"> 
+                        <a href="https://www.instagram.com/vitap.university/?hl=en" class="socialicon"><BiLogoInstagramAlt size={30}/></a>
+                        <a href ="https://www.linkedin.com/school/vitap-university/?originalSubdomain=in"class="socialicon"><AiFillLinkedin size={30}/></a>
+                        <a href ="https://www.facebook.com/vitap.university/"class="socialicon"><BiLogoFacebookCircle size={30}/></a>
+                        <a href ="https://www.youtube.com/c/VITAP"class="socialicon"><AiFillYoutube size={30}/></a>
+                        <a href ="https://twitter.com/VITAPuniversity?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor"class="socialicon"><AiFillTwitterCircle size={30}/></a>
+                    </div>
+                <div class="quicklink pt-20 content-centre">
+                <div class="flex flex-row">
+                <div class="part1">
+                <p class="heading">Quick Links</p>
+                <ul class="list">
+                    <li class="listitem">Academics</li>
+                    <li class="listitem">Programs Offered</li>   
+                    <li class="listitem">Careers</li>
+                    <li class="listitem">Gallery</li>
+                    <li class="listitem">Time of Service</li>
+                    <li class="listitem">Privacy Policy</li>
+                </ul>
+                </div>
+                <div class="part2">
+                <p class="heading">Quick Links</p>
+                <ul class="list">
+                    <li class="listitem">V-TOP Login</li>
+                    <li class="listitem">V-TOP Parent Login </li>   
+                    <li class="listitem">Mail</li>
+                    <li class="listitem">Alumni</li>
+                    <li class="listitem">Contact Us</li>
+                </ul>
+                </div>
+                
+               
+                </div>
+                <div class="flex flex-row">
+               <div class="part1">
+                <p class="heading">Quick Links</p>
+                <ul class="list">
+                <li class="listitem">Rankings</li>
+                    <li class="listitem">Accreditation </li>   
+                    <li class="listitem">Leadership</li>
+                    <li class="listitem">Newsletter</li>
+                    <li class="listitem">Press Media</li>
+                    <li class="listitem">Governance</li>
+                </ul>
+                </div>
+                <div class="part2">
+                <p class="heading">Quick Links</p>
+                <ul class="list">
+                <li class="listitem">Centres</li>
+                    <li class="listitem">Google Scholars</li>   
+                    <li class="listitem">News</li>
+                    <li class="listitem">Patents</li>
+                    <li class="listitem">Projects</li>
+                    <li class="listitem">Publications</li>
+                </ul>
+                </div>
+               
+                </div>
+
+                <div class="flex flex-row">
+               <div class="part1">
+                <p class="heading">Quick Links</p>
+                <ul class="list">
+                    <li class="listitem">Undergraduate</li>
+                    <li class="listitem">Postgraduate</li>   
+                    <li class="listitem">Integrated Degree</li>
+                    <li class="listitem"> Doctoral</li>
+                    <li class="listitem">International</li>
+                </ul>
+                </div>
+                <div class="part2">
+                <p class="heading">Quick Links</p>
+                <ul class="list">
+                <li class="listitem">Public Note</li>
+                    <li class="listitem">Academic Calendar</li>   
+                    <li class="listitem">Campus Maps</li>
+                    <li class="listitem">Application Process</li>
+                    <li class="listitem">Academic Bank of Credits</li>
+                    <li class="listitem">IQAC</li>
+                </ul>
+                </div>
+                </div>
+                </div>
+
+
+                </div>
+                </div>
         </>
     )
 }
