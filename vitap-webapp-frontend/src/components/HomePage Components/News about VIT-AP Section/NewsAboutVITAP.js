d70fd6664a179dbda2ab8a357bde8a220f80445a
Aditi Shaw <89793631+aditishaw2417@users.noreply.github.com>
2023-10-14 16:13:04 +0530

    news section updated

--- vitap-webapp-frontend/src/components/HomePage Components/News about VIT-AP Section/NewsAboutVITAP.js
+++ vitap-webapp-frontend/src/components/HomePage Components/News about VIT-AP Section/NewsAboutVITAP.js
@@ -1,66 +1,38 @@
 import React from "react";
 import Image from "next/image";
-import news1 from "../../../assets/images/newsPhotos/news1.png";
-import circle from "../../../assets/images/newsPhotos/circle.png";
-import arrow from "../../../assets/images/newsPhotos/Vector.png";
+import news2 from "../assets/newsPhotos/3rd Annual Convocation - 2023.jpg";
+import news1 from "../assets/newsPhotos/aimusic.png";
+import news3 from "../assets/newsPhotos/basketball.png";
+import news4 from "../assets/newsPhotos/nationn.png";
+import circle from "../assets/newsPhotos/circle.png";
+import arrow from "../assets/newsPhotos/Vector.png";
 
 const News = () => {
   return (
     <>
-      <div className="bg-[#51060D] h-[964px]">
-        <div className="flex">
-          <div className="ml-[108px] mt-[104px]">
-            <h2 className="text-[#FFFFFF] font-Emilo text-[67px] w-[550px]">
-              News about VIT-AP
-            </h2>
-          </div>
-          <div className="flex justify-end pt-[125px] ml-[674px]">
-            <h1 className="text-[#FFFFFF] text-[25px]">See all</h1>
-          </div>
+      <div className="bg-white h-screen">
+        <div className="flex justify-center items-center">
+          <h2 className="text-[#51060D] font-Emilo text-[60px] w-[550px] mt-[50px] text-center \">
+            News about VIT-AP
+          </h2>
         </div>
-        <div className="flex ml-[108px] mt-[52px]">
-          <div className="w-[392px] h-[639px] bg-[#FFE1E3]">
-            <Image
-              src={news1}
-              width={350}
-              height={200}
-              className="top-[123px] left-[108px] object-cover"
-            />
-            <h3 className="text-[#1B1C1E] font-Emilo font-bold ml-[24px] mt-[21px] text-[30px]">
-              Honorable CM to VIT-AP
-            </h3>
-            <p className="ml-[24px] mt-[21px]">
-              Lorem ipsum dolor sit amet consectetur.Lorem ipsum dolor sit amet
-              consectetur amet consectetur.Lorem ipsum dolor sit amet
-              consectetur.
-            </p>
-            <button className="flex ml-[24px] mt-[87px] rounded-full hover:bg-[#5C0E14] group">
-              <div className="w-[40px] h-[40px] bg-white rounded-full group-hover:w-[190px] group-hover:h-[40px] group-hover:bg-[#5C0E14]"></div>
-              <Image
-                src={arrow}
-                className="absolute mt-[13px] ml-[11px] group-hover:opacity-0"
-              />
-              <p className="ml-[16px] mt-[9px]  font-semibold text-[#FDFEFF] group-hover:ml-[28px] group-hover:mt-[9px] group-hover:absolute">
-                EXPLORE NEWS
-              </p>
-            </button>
-          </div>
-          <div className="w-[392px] h-[639px] bg-[#FFE1E3] ml-[24px]">
+        {/* <div className="flex justify-end pt-[80px] ml-[674px]">
+          <h1 className="text-[#FFFFFF] text-[25px]">See all</h1>
+        </div> */}
+
+        <div className="flex ml-[240px] mt-[20px]">
+          <div className="w-[320px] h-[450px] bg-white ml-[24px] border-b border-gray-300 hover:shadow-2xl hover:border-gray-500">
             <Image
               src={news1}
               width={392}
               height={311}
               className="top-[223px] left-[108px]"
             />
-            <h3 className="text-[#1B1C1E] font-Emilo font-bold ml-[24px] mt-[21px] text-[30px]">
-              Honorable CM to VIT-AP
+            <h3 className="text-[#1B1C1E] font-Emilo font-bold ml-[24px] mt-[21px] text-[20px]">
+              First-ever AI musical concert held at VIT-AP University
             </h3>
-            <p className="ml-[24px] mt-[21px]">
-              Lorem ipsum dolor sit amet consectetur.Lorem ipsum dolor sit amet
-              consectetur amet consectetur.Lorem ipsum dolor sit amet
-              consectetur.
-            </p>
-            <button className="flex ml-[24px] mt-[87px] rounded-full hover:bg-[#5C0E14] group">
+            <p className="ml-[24px] mt-[21px]">Date: 04.10.2023</p>
+            <button className="flex ml-[24px] mt-[50px] rounded-full hover:bg-[#5C0E14] group">
               <div className="w-[40px] h-[40px] bg-white rounded-full group-hover:w-[190px] group-hover:h-[40px] group-hover:bg-[#5C0E14]"></div>
               <Image
                 src={arrow}
@@ -71,22 +43,40 @@ const News = () => {
               </p>
             </button>
           </div>
-          <div className="w-[392px] h-[639px] bg-[#FFE1E3] ml-[24px]">
+          <div className="w-[320px] h-[450px] bg-white ml-[35px] border-b border-gray-300 hover:shadow-2xl hover:border-gray-500">
             <Image
-              src={news1}
+              src={news2}
               width={392}
               height={311}
               className="top-[223px] left-[108px]"
             />
-            <h3 className="text-[#1B1C1E] font-Emilo font-bold ml-[24px] mt-[21px] text-[30px]">
-              Honorable CM to VIT-AP
+            <h3 className="text-[#1B1C1E] font-Emilo font-bold ml-[24px] mt-[21px] text-[20px]">
+              3rd Annual Convocation - 2023
             </h3>
-            <p className="ml-[24px] mt-[21px]">
-              Lorem ipsum dolor sit amet consectetur.Lorem ipsum dolor sit amet
-              consectetur amet consectetur.Lorem ipsum dolor sit amet
-              consectetur.
-            </p>
-            <button className="flex ml-[24px] mt-[87px] rounded-full hover:bg-[#5C0E14] group">
+            <p className="ml-[24px] mt-[21px]">Date: 27.09.2023</p>
+            <button className="flex ml-[24px] mt-[70px] rounded-full hover:bg-[#5C0E14] group">
+              <div className="w-[40px] h-[40px] bg-white rounded-full group-hover:w-[190px] group-hover:h-[40px] group-hover:bg-[#5C0E14]"></div>
+              <Image
+                src={arrow}
+                className="absolute mt-[13px] ml-[11px] group-hover:opacity-0"
+              />
+              <p className="ml-[16px] mt-[9px] text-[#5C0E14] font-semibold hover:text-[#FDFEFF] group-hover:ml-[28px] group-hover:mt-[9px] group-hover:absolute">
+                EXPLORE NEWS
+              </p>
+            </button>
+          </div>
+          <div className="w-[320px] h-[450px] bg-white ml-[35px] border-b border-gray-300 hover:shadow-2xl hover:border-gray-500">
+            <Image
+              src={news3}
+              width={392}
+              height={311}
+              className="top-[223px] left-[108px]"
+            />
+            <h3 className="text-[#1B1C1E] font-Emilo font-bold ml-[24px] mt-[21px] text-[20px]">
+              VIT-AP Men's Basketball team wins gold medal
+            </h3>
+            <p className="ml-[24px] mt-[21px]">Date: 09.09.2023</p>
+            <button className="flex ml-[24px] mt-[25px] rounded-full hover:bg-[#5C0E14] group">
               <div className="w-[40px] h-[40px] bg-white rounded-full group-hover:w-[190px] group-hover:h-[40px] group-hover:bg-[#5C0E14]"></div>
               <Image
                 src={arrow}
