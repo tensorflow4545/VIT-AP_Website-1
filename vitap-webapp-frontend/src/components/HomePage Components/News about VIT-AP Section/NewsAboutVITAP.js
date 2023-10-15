f0791e6cc82a12509dc0ddae546c24ca60d35cf4
Mohith705 <mohith.21bce7511@vitapstudent.ac.in>
2023-10-15 23:33:23 +0530

    research responsiveness done

--- vitap-webapp-frontend/src/components/HomePage Components/News about VIT-AP Section/NewsAboutVITAP.js
+++ vitap-webapp-frontend/src/components/HomePage Components/News about VIT-AP Section/NewsAboutVITAP.js
@@ -1,11 +1,11 @@
 import React from "react";
 import Image from "next/image";
-import news2 from "../assets/newsPhotos/3rd Annual Convocation - 2023.jpg";
-import news1 from "../assets/newsPhotos/aimusic.png";
-import news3 from "../assets/newsPhotos/basketball.png";
-import news4 from "../assets/newsPhotos/nationn.png";
-import circle from "../assets/newsPhotos/circle.png";
-import arrow from "../assets/newsPhotos/Vector.png";
+import news2 from "@/assets/images/newsPhotos/3rd Annual Convocation - 2023.jpg";
+import news1 from "@/assets/images/newsPhotos/aimusic.png";
+import news3 from "@/assets/images/newsPhotos/basketball.png";
+import news4 from "@/assets/images/newsPhotos/nationn.png";
+import circle from "@/assets/images/newsPhotos/circle.png";
+import arrow from "@/assets/images/newsPhotos/Vector.png";
 
 const News = () => {
   return (
