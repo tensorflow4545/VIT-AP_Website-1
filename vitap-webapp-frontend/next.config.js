b7878081c3e92429b4188912b0f5b09ff8790675
kartik kumar <gtgkartik@gmail.com>
2023-10-26 10:46:14 +0530

    changes made to the next config file

--- vitap-webapp-frontend/next.config.js
+++ vitap-webapp-frontend/next.config.js
@@ -1,17 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  server: {
-    // Bind to localhost (your local machine) for development
-    host: 'localhost', // You can also use your IP address here, e.g., '192.168.0.100'
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'http',
-        hostname: 'localhost', // Use the same hostname as your server
-      },
-    ],
+module.exports = {
+  target: 'experimental-serverless-trace',
+  exportPathMap: function () {
+    return {
+      '/': { page: '/' }, // You can add more paths here if needed
+    };
   },
 };
-
-module.exports = nextConfig;
