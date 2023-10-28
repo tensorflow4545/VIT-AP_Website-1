74b5ce2be35b2a2b52e1ff74abb05338be378133
Allenki Akshay <111362177+allenkiakshay@users.noreply.github.com>
2023-10-28 20:20:08 +0530

    update animations of academics and reasearch

--- vitap-webapp-frontend/src/components/HomePage Components/Navbar/NavbarBottom.js
+++ vitap-webapp-frontend/src/components/HomePage Components/Navbar/NavbarBottom.js
@@ -18,7 +18,7 @@ const NavbarBottom = () => {
   useEffect(() => {
     const changeBackground = () => {
       if (typeof window !== "undefined") {
-        if (window.scrollY >= 320) {
+        if (window.scrollY >= 1020) {
           setNavbar(true);
           setLogo(true);
         } else {
@@ -50,7 +50,9 @@ const NavbarBottom = () => {
   };
 
   const [Programmes, setProgrammes] = useState([]);
+  const [Programme, setProgramme] = useState(null);
   const [underline, setunderline] = useState("");
+  const [visible, setVisible] = useState(false);
 
   return (
     <>
@@ -58,9 +60,8 @@ const NavbarBottom = () => {
         {/* {"bg-opacity-50 "} */}
         {/* Increased z-index to 30 */}
         <div
-          className={`bg-gray-900 ${
-            navbar ? "navbar active" : "navbar"
-          }  w-full h-[74px] shadow-xl absolute md:flex font-Montserrant font-medium text-white text-[20px]`}
+          className={`bg-gray-900 ${navbar ? "navbar active" : "navbar"
+            }  w-full h-[74px] shadow-xl absolute md:flex font-Montserrant font-medium text-white text-[20px]`}
         >
           <ul className="flex flex-row  items-center pl-[33px] w-full justify-between pr-[76px]">
             <li className="mr-[100px]">
@@ -70,9 +71,8 @@ const NavbarBottom = () => {
             </li>
 
             <li
-              className={`group hover:cursor-pointer ${
-                activeNavItem === "about" ? "active-nav-item" : ""
-              }`}
+              className={`group hover:cursor-pointer ${activeNavItem === "about" ? "active-nav-item" : ""
+                }`}
               onMouseEnter={() => handleDropdownEnter("about")}
               onMouseLeave={handleDropdownLeave}
             >
@@ -80,7 +80,7 @@ const NavbarBottom = () => {
               <div className="hidden group-hover:block hover:block  w-full h-[566px] absolute top-[55px] box-content  right-0 z-30">
                 <div className="bg-white w-full text-black font-Montserrant h-full relative top-[20px]">
                   <div className="w-full h-full">
-                  <span className="w-[6px] h-[450px] bg-primary opacity-20 rounded-lg absolute left-[430px] top-[45px]"></span>
+                    <span className="w-[6px] h-[450px] bg-primary opacity-20 rounded-lg absolute left-[430px] top-[45px]"></span>
 
                     <ol className="flex-row font-[400] text-[20px]  pt-[82px] pl-[110px] space-y-[29px]">
                       <li className=" custom-list-item   w-[fit-content]">
@@ -125,26 +125,28 @@ const NavbarBottom = () => {
             </li>
 
             <li
-              className={`group hover:cursor-pointer ${
-                activeNavItem === "admissions" ? "active-nav-item" : ""
-              }`}
+              className={`group hover:cursor-pointer ${activeNavItem === "admissions" ? "active-nav-item" : ""
+                }`}
               onMouseEnter={() => handleDropdownEnter("admissions")}
               onMouseLeave={handleDropdownLeave}
             >
               <a className="custom-underline ">Admissions</a>
               <div className="hidden group-hover:block hover:block  w-full h-[566px] absolute top-[55px] box-content  right-0 z-30">
                 <div className="bg-white w-full text-black font-Montserrant h-full relative top-[20px] flex-row flex">
+
                   <ol className="flex-row font-[400] text-[20px]  pt-[82px] pl-[110px] space-y-[29px]">
                     <li
-                      className={`hover:cursor-pointer  w-[fit-content] ${underline}`}
+                      className={`peer/prog hover:cursor-pointer  w-[fit-content] ${underline}`}
                       onMouseEnter={() => {
                         setProgrammes([
                           "Undergraduate",
                           "Dual Degree",
                           "Integrated",
+                          "Postgraduate",
+                          "Ph. D",
                         ]);
                         setunderline(
-                          "underline decoration-[#650010] underline-offset-8 decoration-[6px] "
+                          "custom-underline decoration-[#650010] underline-offset-8 decoration-[6px] "
                         );
                       }}
                     >
@@ -158,41 +160,86 @@ const NavbarBottom = () => {
                     <li>Videos</li>
                     <li>Enquiry</li>
                   </ol>
+                  <span className="w-[6px] h-[450px] bg-primary opacity-20 rounded-lg absolute left-[20%] top-[45px]"></span>
                   <ol
-                    className={` ${""}   flex-row font-[400] text-[20px]  pt-[82px] pl-[110px] space-y-[29px] absolute left-[350px]`}
+                    className={` ${""}   flex-row font-[400] text-[20px]  pt-[82px] pl-[110px] space-y-[29px] absolute left-[350px] `}
                   >
                     {Programmes.map((Program, index) => {
-                      return <li key={index}> {Program} </li>;
+                      return <li key={index} className="custom-list-item w-[fit-content]" onMouseEnter={() => { setProgramme(Program) }}> {Program} </li>;
                     })}
                   </ol>
+                  <span className="w-[6px] h-[450px] bg-primary opacity-20 rounded-lg absolute left-[40%] top-[45px]"></span>
+                  {Programme === "Undergraduate" ? (
+                    <div className="flex">
+                      <ol className="flex-row font-[400] text-[20px] pt-[82px] pl-[650px] space-y-[29px]">
+                        <lh className="font-bold underline decoration-[#000000] underline-offset-8 decoration-[6px] text-[24px]">Engineering</lh>
+                        <li>B.Tech CSE</li>
+                        <li>B.Tech CSBS</li>
+                        <li>B.Tech ECE</li>
+                        <li>B.Tech Mechanical</li>
+                      </ol>
+                      <ol className="flex-row font-[400] text-[20px] pt-[82px] pl-[110px] space-y-[29px]">
+                        <lh className="font-bold underline decoration-[#000000] underline-offset-8 decoration-[6px] text-[24px]">Management</lh>
+                        <li>BBA</li>
+                        <li>B.Com</li>
+                      </ol>
+                    </div>
+                  ) : Programme === "Dual Degree" ? (
+                    <ol className="flex-row font-[400] text-[20px] pt-[82px] pl-[650px] space-y-[29px]">
+                      <li>BA - MA</li>
+                      <li>B.Sc - M.Sc</li>
+                    </ol>) : Programme === "Integrated" ? (
+                      <ol className="flex-row font-[400] text-[20px] pt-[82px] pl-[650px] space-y-[29px]">
+                        <li>M. Tech CSE in Collaboration with Virtusa</li>
+                        <li>M. Tech Software Engineering</li>
+                        <li>BA. LL.B (Hons.)</li>
+                        <li>BBA. LL.B (Hons.)</li>
+                      </ol>
+                    ) : Programme === "Postgraduate" ? (
+                      <ol className="flex-row font-[400] text-[20px] pt-[82px] pl-[650px] space-y-[29px]">
+                        <li>M. Tech VLSI</li>
+                        <li>M.Sc. Data Science</li>
+                        <li>M. Sc. Physics</li>
+                        <li>M. Sc. Chemistry</li>
+                      </ol>
+                    ) : Programme === "Ph. D" ? (
+                      <ol className="flex-row font-[400] text-[20px] pt-[82px] pl-[650px] space-y-[29px]">
+                        <li>School of Computer Science and Engineering</li>
+                        <li>School of Electronics Engineering</li>
+                        <li>School of Mechanical Engineering</li>
+                        <li>School of Advanced Sciences</li>
+                        <li>School of Social Science and Humanities</li>
+                        <li>School of Law</li>
+                        <li>School of Business</li>
+                      </ol>
+                    ) : null}
                 </div>
               </div>
             </li>
 
             <li
-              className={`group hover:cursor-pointer ${
-                activeNavItem === "Academics" ? "active-nav-item" : ""
-              }`}
+              className={`group hover:cursor-pointer ${activeNavItem === "Academics" ? "active-nav-item" : ""
+                }`}
               onMouseEnter={() => handleDropdownEnter("Academics")}
               onMouseLeave={handleDropdownLeave}
             >
               <a className="custom-underline ">Academics</a>
               <div className="hidden group-hover:block hover:block  w-full h-[566px] absolute top-[55px] box-content  right-0 z-30">
                 <div className="bg-white w-full text-black font-Montserrant h-full relative top-[20px]">
-                <div className="w-full h-full flex flex-row ">
+                  <div className="w-full h-full flex flex-row ">
                     <ol className="flex-row font-[400] text-[20px]  pt-[61px] pl-[83px] space-y-[29px]">
                       <li className="font-bold underline decoration-black underline-offset-[2px]">
                         Schools
                       </li>
                       <li>School of Computer Science and Engineering (SCOPE)</li>
                       <li>School of Electronics Engineering (SENSE)</li>
-                       <li>School of Mechanical Engineering (SMEC)</li>
-                        <li>School of Advanced Sciences (SAS)</li>
+                      <li>School of Mechanical Engineering (SMEC)</li>
+                      <li>School of Advanced Sciences (SAS)</li>
                       <li>School of Social Sciences and Humanities (VISH)</li>
                       <li>School of Law (VSL)</li>
                       <li>School of Business (VSB)</li>
                     </ol>
-                    <span className="w-[6px] h-[500px] bg-primary opacity-20 rounded-lg absolute right-[60%] top-[25px]"></span>
+                    <span className="w-[6px] h-[500px] bg-primary opacity-20 rounded-lg absolute right-[68%] top-[25px]"></span>
                     <ol className="flex-row font-[400] text-[20px]  pt-[61px] pl-[200px] space-y-[19px]">
                       <li>Academic Regulations</li>
                       <li>Academic Council</li> <li>Faculty</li> <li>Academic Calender</li>
@@ -210,11 +257,10 @@ const NavbarBottom = () => {
             </li>
 
             <li
-              className={`group hover:cursor-pointer ${
-                activeNavItem === "Career Development Center"
-                  ? "active-nav-item"
-                  : ""
-              }`}
+              className={`group hover:cursor-pointer ${activeNavItem === "Career Development Center"
+                ? "active-nav-item"
+                : ""
+                }`}
               onMouseEnter={() =>
                 handleDropdownEnter("Career Development Center")
               }
@@ -224,7 +270,7 @@ const NavbarBottom = () => {
               <div className="hidden group-hover:block hover:block  w-full h-[566px] absolute top-[55px] box-content  right-0 z-30">
                 <div className="bg-white w-full text-black font-Montserrant h-full relative top-[20px]">
                   <div className="w-full h-full">
-                  <span className="w-[6px] h-[450px] bg-primary opacity-20 rounded-lg absolute left-[430px] top-[45px]"></span>
+                    <span className="w-[6px] h-[450px] bg-primary opacity-20 rounded-lg absolute left-[430px] top-[45px]"></span>
 
                     <ol className="flex-row font-[400] text-[20px]  pt-[82px] pl-[110px] space-y-[29px]">
                       <li className=" custom-list-item   w-[100px]">
@@ -283,9 +329,8 @@ const NavbarBottom = () => {
             </li>
 
             <li
-              className={`group hover:cursor-pointer ${
-                activeNavItem === "Campus Life" ? "active-nav-item" : ""
-              }`}
+              className={`group hover:cursor-pointer ${activeNavItem === "Campus Life" ? "active-nav-item" : ""
+                }`}
               onMouseEnter={() => handleDropdownEnter("Campus Life")}
               onMouseLeave={handleDropdownLeave}
             >
@@ -315,14 +360,14 @@ const NavbarBottom = () => {
                       <li>Guest House</li>
                       <li>Facilities to employees</li>
                     </ol>
-                    <span className="w-[6px] h-[500px] bg-primary opacity-20 rounded-lg absolute right-[57%] top-[25px]"></span>
+                    <span className="w-[6px] h-[500px] bg-primary opacity-20 rounded-lg absolute right-[65%] top-[25px]"></span>
                     <ol className="flex-row font-[400] text-[20px]  pt-[32px] pl-[160px] space-y-[29px]">
                       <li className="font-bold underline decoration-black underline-offset-[2px]">
-                      Committees
+                        Committees
                       </li>
                       <li>V-CHANCE</li>
-                      <li>Anti - Ragging Commitee</li> 
-                      <li>Internal Complaints committee</li> 
+                      <li>Anti - Ragging Commitee</li>
+                      <li>Internal Complaints committee</li>
                       <li>University disciplinary committee</li>
                       <li>Grievances Redressal Commitee</li>
 
@@ -333,15 +378,41 @@ const NavbarBottom = () => {
             </li>
 
             <li
-              className={`group hover:cursor-pointer ${
-                activeNavItem === "Research" ? "active-nav-item" : ""
-              }`}
+              className={`group hover:cursor-pointer ${activeNavItem === "Research" ? "active-nav-item" : ""
+                }`}
               onMouseEnter={() => handleDropdownEnter("Research")}
               onMouseLeave={handleDropdownLeave}
             >
               <a className="custom-underline">Research</a>
               <div className="hidden group-hover:block hover:block  w-full h-[566px] absolute top-[55px] box-content  right-0 z-30">
-                <div className="bg-white w-full text-black font-Montserrant h-full relative top-[20px]"></div>
+                <div className="bg-white w-full text-black font-Montserrant h-full relative top-[20px]">
+                <ol className="flex-row font-[400] text-[20px]  pt-[82px] pl-[110px] space-y-[29px]">
+                  <li>Academic Reasearch</li>
+                  <li>URE Project</li>
+                  <li className="peer/coe custom-list-item w-[fit-content]">Center of Excellence</li>
+                  <li className="absolute left-[500px] invisible top-[20px] peer-hover/coe:visible">
+                    <ol className="flex-row font-[400] text-[20px] pt-[20px] space-y-[29px]">
+                      <li>AI & Robotics</li>
+                      <li>Cyber-Security</li>
+                      <li>Internet of Things</li>
+                      <li>Blockchain</li>
+                    </ol>
+                  </li>
+                  <li className="peer/iiec custom-list-item w-[fit-content]">IIEC</li>
+                  <li className="absolute left-[500px] invisible top-[20px] peer-hover/iiec:visible">
+                    <ol className="flex-row font-[400] text-[20px] pt-[20px] space-y-[29px]">
+                      <li>V-Launch</li>
+                      <li>RGEMS</li>
+                      <li>SpoRIC</li>
+                      <li>Patents</li>
+                      <li>Incubatees</li>
+                      <li>E- magazine</li>
+                    </ol>
+                  </li>
+                  <li>VITBIF</li>
+                </ol>
+                <span className="w-[6px] h-[450px] bg-primary opacity-20 rounded-lg absolute left-[20%] top-[45px]"></span>
+                </div>
               </div>
             </li>
           </ul>
