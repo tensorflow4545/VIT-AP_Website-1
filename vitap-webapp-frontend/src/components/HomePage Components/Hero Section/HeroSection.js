877dffa8fcad093ee7b47ce1f408cef3b52d1305
kartik kumar <gtgkartik@gmail.com>
2023-11-06 00:26:32 +0530

    minor fixes

--- vitap-webapp-frontend/src/components/HomePage Components/Hero Section/HeroSection.js
+++ vitap-webapp-frontend/src/components/HomePage Components/Hero Section/HeroSection.js
@@ -5,7 +5,6 @@ import headerimage2 from "../../../assets/images/Homepage Images/HeaderImage2.pn
 import { BsArrowRight } from "react-icons/bs";
 import config from "@/config";
 import { Carousel } from "@material-tailwind/react";
-import Video from "./Video";
 
 // import { Carousel } from "@material-tailwind/react";
 
@@ -61,7 +60,7 @@ const HeroSection = () => {
       </Carousel>
 
       <div className="z-0 absolute ls:top-[240px] md:top-[352px] ls:left-[108px] sm:left-[150px] text-white  font-semibold ls:w-[500px] md:w-[800px] sm:top-[200px]  ls:block sm:flex-col ls:text-left sm:text-center">
-        <h1 className="md:leading-[100px] font-Emilio sm:leading-10 ls:leading-[60px] sm:text-[35px] ls:text-[50px] md:text-[78px] ">
+        <h1 className="md:leading-[100px] font-Montserrat sm:leading-10 ls:leading-[60px] sm:text-[35px] ls:text-[50px] md:text-[78px] ">
           Welcome to <br /> VIT-AP University
         </h1>
         <h1 className="md:text-[20px] sm:text-[10px] ls:text-[15px] font-Montserrant">
