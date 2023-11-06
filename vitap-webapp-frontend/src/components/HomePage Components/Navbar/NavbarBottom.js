23878f9086b7fa861051b1e3fb5a34c14440982f
Allenki Akshay <111362177+allenkiakshay@users.noreply.github.com>
2023-11-06 19:07:05 +0530

    Update NavbarBottom.js

--- vitap-webapp-frontend/src/components/HomePage Components/Navbar/NavbarBottom.js
+++ vitap-webapp-frontend/src/components/HomePage Components/Navbar/NavbarBottom.js
@@ -170,7 +170,7 @@ const NavbarBottom = () => {
                           "Ph. D",
                         ]);
                         setunderline(
-                          "custom-list-item decoration-[#650010] underline-offset-8 decoration-[6px] "
+                          "custom-underline decoration-[#650010] underline-offset-8 decoration-[6px] "
                         );
                       }}
                     >
