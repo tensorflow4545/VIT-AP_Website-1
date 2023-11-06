7d38d7f288115a198e8bdca5b7a66e9b7edbdbd5
Allenki Akshay <111362177+allenkiakshay@users.noreply.github.com>
2023-11-06 18:34:41 +0530

    Update NewsAboutVITAP.js

--- vitap-webapp-frontend/src/components/HomePage Components/News about VIT-AP Section/NewsAboutVITAP.js
+++ vitap-webapp-frontend/src/components/HomePage Components/News about VIT-AP Section/NewsAboutVITAP.js
@@ -29,8 +29,8 @@ const News = () => {
           <h1 className="text-[#FFFFFF] text-[25px]">See all</h1>
         </div> */}
 
-        <div className="flex justify-center mt-[20px] space-x-[80px]">
-          <div className="w-[320px]  hover:shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)] cursor-pointer  h-[450px] bg-white ml-[24px] border-b border-gray-300  hover:border-gray-500">
+          <div className="flex ml-[240px] mt-[20px]">
+          <div className="w-[320px] h-[450px] bg-white ml-[24px] border-b shadow-2xl border-gray-800 section">
             <Image
               alt="A descriptive text here"
               src={news2}
@@ -54,7 +54,7 @@ const News = () => {
               </p>
             </button>
           </div>
-          <div className="w-[320px] hover:shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)] cursor-pointer  h-[450px] bg-white ml-[35px] border-b border-gray-300  hover:border-gray-500">
+          <div className="w-[320px] h-[450px] bg-white ml-[35px] border-b shadow-2xl border-gray-800 section">           
             <Image
               alt="A descriptive text here"
               src={news2}
