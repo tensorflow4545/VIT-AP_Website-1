1c197d9d23bd23238522c118490a8589fb7b28e3
kartik kumar <gtgkartik@gmail.com>
2023-11-07 03:00:44 +0530

    Added page routes in the app directory for /convocation, /schools, and /announcements

---
+++ vitap-webapp-frontend/src/app/announcements/page.js
@@ -0,0 +1,15 @@
+import AnnouncementsSection from "@/components/AnnouncementsPage Components/Announcements Section/AnnouncementsSection";
+import Footer from "@/components/HomePage Components/Footer Section/Footer";
+import NavbarBottom from "@/components/HomePage Components/Navbar/NavbarBottom";
+
+const AnnouncementsPage = () => {
+  return (
+    <>
+      <NavbarBottom />
+      <AnnouncementsSection />
+      <Footer />
+    </>
+  );
+};
+
+export default AnnouncementsPage;
