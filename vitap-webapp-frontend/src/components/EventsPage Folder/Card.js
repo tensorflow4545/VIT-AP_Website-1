47d9e44509555595fbc47a1178830a656c4ca491
Aditi Shaw <89793631+aditishaw2417@users.noreply.github.com>
2023-11-07 12:01:59 +0530

    fixed fonts

--- vitap-webapp-frontend/src/components/EventsPage Folder/Card.js
+++ vitap-webapp-frontend/src/components/EventsPage Folder/Card.js
@@ -3,8 +3,8 @@ import React, { useState } from "react";
 import Datetime from "react-datetime";
 import "react-datetime/css/react-datetime.css";
 import Image from "next/image";
-import banner from "../../assets/images/Events/banner2.png";
-import card from "../../assets/images/Events/card.png";
+import banner from "@/assets/images/Events/banner2.png";
+import card from "@/assets/images/Events/card.png";
 //import "react-datepicker/dist/react-datepicker.css";
 
 const Card = () => {
@@ -25,7 +25,9 @@ const Card = () => {
               transform: "translate(-50%, -50%)",
             }}
           >
-            <p className="text-white text-4xl font-bold">Events</p>
+            <p className="text-white text-4xl font-bold font-Emilo text-[61px]">
+              Events
+            </p>
           </div>
         </div>
         <div className="flex flex-row relative">
@@ -38,19 +40,19 @@ const Card = () => {
             />
           </div>
           <div className="w-[370px] h-[370px] mt-[100px] ml-[60px] font-Montserrant text-[18px]">
-            <p className="mb-4">
+            <p className="mb-4 font-Montserrant">
               <strong>Event name:</strong> V-Launch Pad
             </p>
-            <p className="mb-4">
+            <p className="mb-4 font-Montserrant">
               <strong>Event Date and Time:</strong> 18th October, 4:00pm
             </p>
-            <p className="mb-4">
+            <p className="mb-4 font-Montserrant">
               <strong>Venue:</strong> Newton Hall
             </p>
-            <p className="mb-4">
+            <p className="mb-4 font-Montserrant">
               <strong>Registration Link:</strong> Registration Link
             </p>
-            <p className="mb-4">
+            <p className="mb-4 font-Montserrant">
               <strong>About the Event:</strong> Lorem ipsum dolor sit amet
               consectetur. Morbi tortor tempus aliquet ales.Lorem ipsum dolor
               sit amet consectetur.
@@ -72,24 +74,24 @@ const Card = () => {
               }}
               className="flex flex-col rounded items-center border border-black shadow-2xl mt-[20px]"
             >
-              <p className="text-center mt-[20px] font-extrabold">
+              <p className="text-center mt-[20px] font-extrabold font-Inter">
                 Upcoming Events
               </p>
               <hr className="w-[230px] mt-4" />
-              <h5 className="font-bold mt-3">V-Launch Pad</h5>
-              <p>18th Oct, 4pm</p>
+              <h5 className="font-bold mt-3 font-Inter">V-Launch Pad</h5>
+              <p className="font-Inter">18th Oct, 4pm</p>
               <hr className="w-[230px] mt-4" />
-              <h5 className="font-bold mt-3">V-Launch Pad</h5>
-              <p>18th Oct, 4pm</p>
+              <h5 className="font-bold mt-3 font-Inter">V-Launch Pad</h5>
+              <p className="font-Inter">18th Oct, 4pm</p>
               <hr className="w-[230px] mt-4" />
-              <h5 className="font-bold mt-3">V-Launch Pad</h5>
-              <p>18th Oct, 4pm</p>
+              <h5 className="font-bold mt-3 font-Inter">V-Launch Pad</h5>
+              <p className="font-Inter">18th Oct, 4pm</p>
               <hr className="w-[230px] mt-4" />
-              <h5 className="font-bold mt-3">V-Launch Pad</h5>
-              <p>18th Oct, 4pm</p>
+              <h5 className="font-bold mt-3 font-Inter">V-Launch Pad</h5>
+              <p className="font-Inter">18th Oct, 4pm</p>
               <hr className="w-[230px] mt-4" />
-              <h5 className="font-bold mt-3">V-Launch Pad</h5>
-              <p>18th Oct, 4pm</p>
+              <h5 className="font-bold mt-3 font-Inter">V-Launch Pad</h5>
+              <p className="font-Inter">18th Oct, 4pm</p>
             </div>
           </div>
         </div>
