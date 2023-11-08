4e377bcf8a221a2824660e8e672c5856efec02fa
Mohith705 <mohith.21bce7511@vitapstudent.ac.in>
2023-11-08 20:13:50 +0530

    fixed view more animation and added faculty page

--- vitap-webapp-frontend/src/components/HomePage Components/News about VIT-AP Section/NewsAboutVITAP.js
+++ vitap-webapp-frontend/src/components/HomePage Components/News about VIT-AP Section/NewsAboutVITAP.js
@@ -13,17 +13,17 @@ const News = () => {
   return (
     <>
       <div className="bg-white h-screen">
-      <div className="flex items-center justify-between">
-  <h2 className="text-[#51060D] font-Emilo text-[60px] mt-[50px] ml-[100px] mb-[10px] text-left">
-    News about VIT-AP
-  </h2>
-  <div className="flex items-center justify-center mr-[50px] mt-10">
-  <div class="Rectangle91 w-[50px] h-[30px] absolute bg-[#5C0E14] opacity-20 transition-transform hover:right-[90px] hover:w-[125px] "></div>
-    <h1 className="text-[#5C0E14]">View More &nbsp;</h1>
-  <div class="Arrow w-[50px] border-2 border-solid border-[#5C0E14]"></div>
-</div>
+        <div className="flex items-center justify-between">
+          <h2 className="text-[#51060D] font-Emilo text-[60px] mt-[50px] ml-[100px] mb-[10px] text-left">
+            News about VIT-AP
+          </h2>
+          <div className="flex items-center justify-center mr-[50px] mt-10 group">
+            <div class="Rectangle91 w-[50px] h-[30px] absolute bg-[#5C0E14] opacity-20 transition-transform group-hover:right-[90px] group-hover:w-[125px] group-hover:transition-all group-hover:duration-300"></div>
+            <h1 className="text-[#5C0E14]">View More &nbsp;</h1>
+            <div class="Arrow w-[50px] border-2 border-solid border-[#5C0E14]"></div>
+          </div>
 
-</div>
+        </div>
 
         {/* <div className="flex justify-end pt-[80px] ml-[674px]">
           <h1 className="text-[#FFFFFF] text-[25px]">See all</h1>
