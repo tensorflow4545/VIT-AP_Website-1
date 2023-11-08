5257adc48d3f5c24be77147b5aafef63cf3a9c1f
vec.sripto@gmail.com <vec.sripto@gmail.com>
2023-11-08 11:38:35 +0000

    change

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@ git pull
 cd vitap-webapp-frontend
 npm install
 npm run build
-ln -s out ~/Vitiebot/static
+ln -s ~/VIT-AP_Website-1/vitap-webapp-frontend/out ~/Vitiebot/static
 cd ..
 sudo service apache2 restart
 
