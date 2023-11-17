ef8e8e12725745ebbd8cc4ad2920f0444ee1f92f
Mohith705 <mohith.21bce7511@vitapstudent.ac.in>
2023-11-17 13:04:32 +0530

    modified css in library page and added few compoments in campus life page

---
+++ vitap-webapp-frontend/src/app/campuslife/page.js
@@ -0,0 +1,22 @@
+import CampusBanner from '@/components/CampusLife Components/CampusBanner Section/CampusBanner'
+import CampusHeader from '@/components/CampusLife Components/CampusHeader Section/CampusHeader'
+import Component from '@/components/HomePage Components/Footer Section/Component'
+import Footer from '@/components/HomePage Components/Footer Section/Footer'
+import NavbarBottom from '@/components/HomePage Components/Navbar/NavbarBottom'
+import NavbarUpper from '@/components/HomePage Components/Navbar/NavbarUpper'
+import React from 'react'
+
+const page = () => {
+    return (
+        <div>
+            <NavbarUpper />
+            <NavbarBottom />
+            <CampusHeader />
+            <CampusBanner />
+            <Component />
+            <Footer />
+        </div>
+    )
+}
+
+export default page
