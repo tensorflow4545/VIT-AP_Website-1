d672c20d1de987fc5a748ce0b641175375f32216
Aditi Shaw <89793631+aditishaw2417@users.noreply.github.com>
2023-11-22 19:34:48 +0530

    image path set

--- vitap-webapp-frontend/src/components/SchoolPage Components/testimonials folder/Testimonials.js
+++ vitap-webapp-frontend/src/components/SchoolPage Components/testimonials folder/Testimonials.js
@@ -1,5 +1,5 @@
 import React from "react";
-import img from "../../assets/images/testimonials/img2.png";
+import img from "../../../assets/images/testimonials/img2.png";
 import Image from "next/image";
 
 const Testimonials = () => {
