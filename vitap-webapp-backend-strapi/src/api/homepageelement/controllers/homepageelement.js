51f9666c39538d9fa5873063e3847076aeba59f3
kartik kumar <gtgkartik@gmail.com>
2023-11-24 15:32:48 +0530

    removed strapi application

--- vitap-webapp-backend-strapi/src/api/homepageelement/controllers/homepageelement.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * homepageelement controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::homepageelement.homepageelement');
