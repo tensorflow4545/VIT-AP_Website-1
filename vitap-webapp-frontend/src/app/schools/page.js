19504c383e083a5c48dfefee51764a78d7875734
allenki_akshay <allenkiakshay8322@gmail.com>
2023-12-05 23:10:48 +0530

    update school page

--- vitap-webapp-frontend/src/app/schools/page.js
+++ vitap-webapp-frontend/src/app/schools/page.js
@@ -2,6 +2,7 @@ import Footer from "@/components/HomePage Components/Footer Section/Footer";
 import NavbarBottom from "@/components/HomePage Components/Navbar/NavbarBottom";
 import NavbarUpper from "@/components/HomePage Components/Navbar/NavbarUpper";
 import ProgrammesOffered from "@/components/HomePage Components/Programmes Offered Section/ProgrammesOffered";
+import News from "@/components/HomePage Components/News about VIT-AP Section/NewsAboutVITAP";
 import AcheivementsSection from "@/components/SchoolPage Components/Acheivements Section/AcheivementsSection";
 import FacultySection from "@/components/SchoolPage Components/Faculty Section/FacultySection";
 import ContactSection from "@/components/SchoolPage Components/Contact Section/contact";
@@ -14,6 +15,8 @@ import FotterComponent from "@/components/HomePage Components/Footer Section/Com
 import Programoffered from "@/components/SchoolPage Components/Program Offered Section/Programoffered";
 import NavbarUpperPages from "@/components/HomePage Components/Navbar/NavbarUpperPages";
 import NavbarBottomPages from "@/components/HomePage Components/Navbar/NavbarBottomPages";
+import Testimonials from "@/components/SchoolPage Components/testimonials folder/Testimonials";
+import ResearchInnovation from "@/components/HomePage Components/Research and Innovation Section/ResearchInnovation";
 
 const SchoolsPage = () => {
     return ( 
@@ -26,9 +29,13 @@ const SchoolsPage = () => {
             <Programoffered/>
             <FacultySection/>
             <AcheivementsSection/>
+            <News/>
+            <ResearchInnovation/>
             <Mous/>
+            <Testimonials/>
             <GallerySection/>
             <ContactSection/>
+            <FotterComponent/>
             <Footer/>
         </>
      );
