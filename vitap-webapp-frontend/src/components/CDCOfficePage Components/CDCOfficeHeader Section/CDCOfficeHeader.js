1de088809d67309755aeb3c6ee9bf520609b1695
Mohith705 <mohith.21bce7511@vitapstudent.ac.in>
2023-12-06 16:00:55 +0530

    completed mobile view of CDC Office Page

--- vitap-webapp-frontend/src/components/CDCOfficePage Components/CDCOfficeHeader Section/CDCOfficeHeader.js
+++ vitap-webapp-frontend/src/components/CDCOfficePage Components/CDCOfficeHeader Section/CDCOfficeHeader.js
@@ -7,7 +7,7 @@ const CDCOfficeHeader = () => {
                 style={{ backgroundImage: `url("/announcements.png")` }}
                 className="relative max-w-[1560px] w-full h-[300px] bg-center mx-auto flex items-end justify-start"
             >
-                <h1 className="text-white text-[45px] font-semibold text-center flex justify-center items-center p-[50px]">
+                <h1 className="text-white text-[32px] lx:text-[45px] font-semibold text-center flex justify-center items-center p-[50px]">
                     CDC Office
                 </h1>
             </div>
