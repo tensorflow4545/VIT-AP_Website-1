fa72d0cd7939fa2afb1db669882c97826198c55a
allenki_akshay <allenkiakshay8322@gmail.com>
2023-12-12 17:14:38 +0530

    update 360 view and allumini link in navbar

--- vitap-webapp-frontend/src/components/HomePage Components/Navbar/NavbarUpper.js
+++ vitap-webapp-frontend/src/components/HomePage Components/Navbar/NavbarUpper.js
@@ -7,6 +7,7 @@ import { useState, useEffect } from "react";
 import "./NavbarUpper.css";
 import { RxHamburgerMenu } from "react-icons/rx";
 import Image from "next/image";
+import Link from "next/link";
 
 const navlinks = [];
 // import axios from 'axios';
@@ -138,7 +139,7 @@ const NavbarUpper = () => {
 
         <ul className="flex font-Montserrant h-[20px] md:space-x-[31px] ls:space-x-[20px]  text-[16px] ">
           <li>
-            <a>Alumini</a>
+            <a href="https://alumni.vitap.ac.in/" target="_blank" rel="noopener noreferrer">Alumini</a>
           </li>
           <li>
             <a>VIT-AP Advantages</a>
@@ -150,7 +151,7 @@ const NavbarUpper = () => {
             </a>
           </li>
           <li>
-            <a>
+            <a href="/Campustour/index04.html" target="_blank" rel="noopener noreferrer">
               <Tb360View size={25} />
             </a>
           </li>
