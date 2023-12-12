6c8ed6cc3267d6dd2a6f108422cb361839363228
Mohith705 <mohith.21bce7511@vitapstudent.ac.in>
2023-12-12 15:24:18 +0530

    create advantages page and added few compnents

--- vitap-webapp-frontend/src/components/SchoolPage Components/News Section/News.js
+++ vitap-webapp-frontend/src/components/SchoolPage Components/News Section/News.js
@@ -10,21 +10,19 @@ export default function News() {
       desc: "Lorem ipsum dolor sit amet consectetur.Lorem ipsum dolor sit amet consectetur amet consectetur."
     },
     {
-      img:"/news.png",
-      head:"Meet and Greet’23",
-      desc:"Lorem ipsum dolor sit amet consectetur.Lorem ipsum dolor sit amet consectetur amet consectetur."
-   },
-   {
-    img:"/news.png",
-    head:"Meet and Greet’23",
-    desc:"Lorem ipsum dolor sit amet consectetur.Lorem ipsum dolor sit amet consectetur amet consectetur."
- }
-];
-    return(
-      <>
-        <div class="w-full h-[800px] bg-white">
-
-      
+      img: "/news.png",
+      head: "Meet and Greet’23",
+      desc: "Lorem ipsum dolor sit amet consectetur.Lorem ipsum dolor sit amet consectetur amet consectetur."
+    },
+    {
+      img: "/news.png",
+      head: "Meet and Greet’23",
+      desc: "Lorem ipsum dolor sit amet consectetur.Lorem ipsum dolor sit amet consectetur amet consectetur."
+    }
+  ];
+  return (
+    <>
+      <div class="w-full h-[800px] bg-white">
         <div class="w-full h-[400px] bg-red-100 pt-[66px] px-[108px]">
           <p class="text-[48px] text-red-950 font-bold text-emilio">News and Events</p>
           <div className="flex flex-row relative items-center float-right w-[100px] transition-all duration-300 ease-in group mr-[30px] ">
@@ -36,37 +34,37 @@ export default function News() {
           </div>
           {/* flex */}
           <div class="flex flex-row pt-[63px]">
-            
-            {News.map((news)=>{
-               return <div>
-               {/* card */}
-               
-           <div class="basis w-[350px] h-[450px] bg-white rounded-sm mr-[24px] trasnition duration-1000 hover:-translate-y-5 ">
-           <div class="w-[350px] h-[241px] relative">
 
-            <Image class="absolute z-0" width={350} height={241} src={news.img} alt="Event Image"></Image>
-            <div class="w-[53px] h-[72px] bg-red-900 absolute z-10 right-5 top-7 rounded-md">
-         
-                <p class="text-[16px] text-white text-emilio font-semibold pt-[8px] px-[13px]">Oct</p>
-                <p class="text-[28px] text-white text-emilio font-semibold px-[11px]">23</p>
-            </div>           
+            {News.map((news) => {
 
-           </div>
+              return (<div>
+                {/* card */}
+
+                <div class="basis w-[350px] h-[450px] bg-white rounded-sm mr-[24px] trasnition duration-1000 hover:-translate-y-5 ">
+                  <div class="w-[350px] h-[241px] relative">
+
+                    <Image class="absolute z-0" width={350} height={241} src={news.img} alt="Event Image"></Image>
+                    <div class="w-[53px] h-[72px] bg-red-900 absolute z-10 right-5 top-7 rounded-md">
+
+                      <p class="text-[16px] text-white text-emilio font-semibold pt-[8px] px-[13px]">Oct</p>
+                      <p class="text-[28px] text-white text-emilio font-semibold px-[11px]">23</p>
+                    </div>
+
+                  </div>
+
+
+                  <p class="text-[28px] text-dark font-[600px] text-emilio pt-[16px]">{news.head}</p>
+                  <p class="text-[18px] text-zinc-400 text-montserrat font-[400px] pt-[9px]">{news.desc}</p>
+                  <p class="text-[18px] text-red-950 font-medium text-montserrat pt-[16px]">Know More</p>
+                </div>
+              </div>
+
+              )
             })}
-
-
-           <p class="text-[28px] text-dark font-[600px] text-emilio pt-[16px]">{news.head}</p>
-           <p class="text-[18px] text-zinc-400 text-montserrat font-[400px] pt-[9px]">{news.desc}</p>
-           <p class="text-[18px] text-red-950 font-medium text-montserrat pt-[16px]">Know More</p>
-           </div>
-           </div>
           </div>
 
         </div>
-
-      </>
-    )
+      </div>
+    </>
+  )
 }
-
-export default News;
-
