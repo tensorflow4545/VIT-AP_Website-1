ee6c43f2eadd253a998ee7a8b855cce690a68e13
allenki_akshay <allenkiakshay8322@gmail.com>
2023-12-16 17:04:43 +0530

    updates

--- vitap-webapp-frontend/src/components/HomePage Components/CDC Section/CDCHeader Section/CDCHeader.js
+++ vitap-webapp-frontend/src/components/HomePage Components/CDC Section/CDCHeader Section/CDCHeader.js
@@ -1,6 +1,7 @@
 "use client";
+
 import Image from 'next/image'
-import React, { useState,useEffect } from 'react';
+import React, { useState } from 'react';
 import "./SuperdreamOffers.css"
 import { homeCDC } from '@/constants';
 import { MdNavigateNext } from "react-icons/md";
@@ -8,41 +9,7 @@ import { GrFormPrevious } from "react-icons/gr";
 
 const CDCHeader = () => {
 
-    // const [index, setIndex] = useState(0);
-    const images = [
-        '/package.png',
-        '/super-dream-offer 1.png',
-        '/package.png',
-        '/package.png',
-        '/package.png',
-        '/package.png',
-        '/package.png',
-        '/package.png',
-        '/package.png',
-        '/package.png',
-        '/package.png',
-        '/package.png',
-        '/package.png',
-        '/package.png',
-        '/package.png',
-        '/package.png',
-    ];
-
-    const [currentIndex, setCurrentIndex] = useState(0);
-
-    useEffect(() => {
-        const intervalId = setInterval(() => {
-          setCurrentIndex((prevIndex) => (prevIndex + 1) % homeCDC.length);
-        }, 3000);
-    
-        return () => {
-          clearInterval(intervalId);
-        };
-      }, [currentIndex, homeCDC.length]);
-
-    if (!homeCDC.length || currentIndex < 0 || currentIndex >= homeCDC.length) {
-        return null; // or handle the case accordingly
-      };
+    const [index, setIndex] = useState(0);
 
     return (
         <>
@@ -52,12 +19,12 @@ const CDCHeader = () => {
                 <div className='max-w-[1400px] w-[100%] min-h-[420px] h-[100%] p-5 flex '>
                     <div className='flex-1 flex justify-start items-center'>
                         <div className='max-w-[400px] min-h-[370px] h-[100%] w-[100%] relative'>
-                            <Image src={homeCDC[currentIndex].imgUrl} alt='glance' fill className='p-7' />
+                            <Image src={homeCDC[index].imgUrl} alt='glance' fill className='p-7' />
                             <div className='absolute right-0 bottom-0 max-h-[50px] max-w-[50px] w-[100%] h-[100%] border-4 border-[#650010] border-t-0 border-l-0'></div>
                         </div>
                     </div>
                     <div className='flex-1 flex flex-col justify-center items-start gap-2 mr-[0px] lx:-ml-[100px] md:-ml-[200px] lx:mr-[20px]md:mr-[50px]'>
-                        <h1 className='text-[#000] text-[38px] font-bold font-Emilio'>Placement Spotlight</h1>
+                        <h1 className='text-[#000] text-[38px] font-bold font-Emilio'>Number At Glance</h1>
                         <p className='text-[#000] text-[18px] '>Lorem ipsum dolor sit amet consectetur.Lorem ipsum dolor sit amet consectetur.Lorem ipsum dolor sit amet consectetur.Lorem ipsum dolor sit amet onsectetur.Lorem ipsum dolor sit amet consectetur.Lorem ipsum dolor sit amet consectetur.</p>
                     </div>
                 </div>
@@ -71,7 +38,7 @@ const CDCHeader = () => {
                 <div className='absolute max-w-[300px] w-[100%] max-h-[50px] h-[100%] right-36 top-0 bg-[#650010] text-white text-[22px] flex justify-center items-start'>
                     <button>Top Recruiters</button>
                 </div>
-                {/* <div className='flex justify-center items-center gap-2 slide-track'>
+                <div className='flex justify-center items-center gap-2 slide-track'>
                     <div className='lx:min-w-[200px] w-[100%] lx:min-h-[150px] h-[100%] min-w-[180px] min-h-[130px] relative slide overflow-hidden group transition-all duration-300 ease-in-out' onClick={() => setIndex(0)}>
                         <Image src={"/package.png"} fill alt='package' className='group-hover:scale-110' />
                     </div>
@@ -128,20 +95,6 @@ const CDCHeader = () => {
                         <Image src={"/package.png"} fill alt='package' className='group-hover:scale-110' />
                     </div>
 
-                </div> */}
-                <div className='flex justify-center items-center gap-2 slide-track'>
-                    {images.map((src, index) => (
-                        <div
-                            key={index}
-                            className={`lx:min-w-[200px] w-[100%] lx:min-h-[150px] h-[100%] min-w-[180px] min-h-[130px] relative slide overflow-hidden group transition-all duration-300 ease-in-out`}
-                            onClick={() => {
-                                // handleImageClick(index);
-                                setCurrentIndex(index);
-                            }}
-                        >
-                            <Image src={src} fill alt={`Image ${index + 1}`} className='group-hover:scale-110' />
-                        </div>
-                    ))}
                 </div>
             </div>
         </>
