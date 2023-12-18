7699c2a8cf8e8ad217636c06b8dd69ee461f923d
Anshul Ojha <128066014+tensorflow4545@users.noreply.github.com>
2023-12-18 21:05:19 +0530

    Update Testtimonial.js

--- vitap-webapp-frontend/src/components/hostel page/Testimonial section/Testtimonial.js
+++ vitap-webapp-frontend/src/components/hostel page/Testimonial section/Testtimonial.js
@@ -1,11 +1,11 @@
 export default function testimonial(){
     return(
         <>
-            <div class="w-full h-[250px] bg-red-900">
-            <div class="pt-[78px] pb-[59px] px-[300px]">
-              <p class="text-white text-[28px] font-[700px] tetx-emilio text-center">“ Lorem ipsum dolor sit amet consectetur, Lorem ipsum</p>          
-              <p class="text-white text-[28px] font-[700px] tetx-emilio text-center">dolor sit amet consectetur ”</p>
-              <p class="text-[24] font-[400px] text-white text-right">-Jagadish Mudiganti</p>
+            <div className="w-full h-[250px] bg-red-900">
+            <div className="pt-[78px] pb-[59px] px-[300px]">
+              <p className="text-white text-[28px] font-[700px] tetx-emilio text-center">“ Lorem ipsum dolor sit amet consectetur, Lorem ipsum</p>          
+              <p className="text-white text-[28px] font-[700px] tetx-emilio text-center">dolor sit amet consectetur ”</p>
+              <p className="text-[24] font-[400px] text-white text-right">-Jagadish Mudiganti</p>
             </div>
             </div>
         </>
