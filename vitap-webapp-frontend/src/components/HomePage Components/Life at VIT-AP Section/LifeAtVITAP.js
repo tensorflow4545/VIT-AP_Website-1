0e2498c4bc0c165407d42deb55d54b0a35614d8a
Aditi Shaw <89793631+aditishaw2417@users.noreply.github.com>
2023-12-22 18:41:09 +0530

    Life at Vitap page styles included for mobile view

--- vitap-webapp-frontend/src/components/HomePage Components/Life at VIT-AP Section/LifeAtVITAP.js
+++ vitap-webapp-frontend/src/components/HomePage Components/Life at VIT-AP Section/LifeAtVITAP.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Image from "next/image";
-import styles from "../../../styles/Life.module.css";
+import styles from "@/styles/Life.module.css";
 import exp1 from "@/assets/images/explorePhotos/explore1.png";
 import exp2 from "@/assets/images/explorePhotos/explore2.png";
 import exp3 from "@/assets/images/explorePhotos/explore3.png";
@@ -10,7 +10,8 @@ import exp6 from "@/assets/images/explorePhotos/explore6.png";
 import exp7 from "@/assets/images/explorePhotos/explore7.png";
 import exp8 from "@/assets/images/explorePhotos/explore8.png";
 import exp9 from "@/assets/images/explorePhotos/explore9.png";
-import Link from "next/link";
+import mob from "@/assets/images/explorePhotos/mobile.png";
+import Link from "next/Link";
 
 const Life = () => {
   return (
@@ -24,6 +25,22 @@ const Life = () => {
           height={200}
           className={`${styles.img1} absolute top-3 left-[100px]`}
         />
+        <Image
+          src={exp2}
+          className={`${styles.mobile1} opacity-0 absolute top-0 left-[520px]`}
+        />
+        <Image
+          src={exp2}
+          className={`${styles.mobile7} opacity-0 absolute top-0 left-[520px]`}
+        />
+        <Image
+          src={exp2}
+          className={`${styles.mobile8} opacity-0 absolute top-0 left-[520px]`}
+        />
+        <Image src={mob} className={`${styles.mobile3} opacity-0`} />
+        <Image src={mob} className={`${styles.mobile4} opacity-0`} />
+        <Image src={mob} className={`${styles.mobile5} opacity-0`} />
+        <Image src={mob} className={`${styles.mobile6} opacity-0`} />
         <Image
           src={exp2}
           width={184}
@@ -58,44 +75,45 @@ const Life = () => {
           src={exp7}
           width={272}
           height={200}
-          className={`${styles.img7} absolute left-[10%] bottom-[50px]`}
+          className={`${styles.img7} absolute left-[10%] bottom-[10px]`}
         />
         <Image
           src={exp8}
           width={280}
           height={202}
-          className={`${styles.img8} absolute bottom-[10px] left-[41%]`}
-
+          className={`${styles.img8} absolute bottom-0 left-[41%]`}
         />
         <Image
           src={exp9}
           width={280}
           height={202}
-          className={`${styles.img9} absolute bottom-[20px] right-[10%]`}
+          className={`${styles.img9} absolute bottom-[10px] right-[10%]`}
         />
-        <h4
-          className={`${styles.explore} mt-[210px] text-[30px] text-center items-center relative text-[#5C0E14] font-Emilio font-bold`}
+        <div
+          className={`${styles.centerdiv} flex items-center justify-center mt-[-330px]`}
         >
-          <i>Explore our campus</i>
-        </h4>
-        <h1
-          className={`${styles.life} text-[80px] mt-[5px] text-center items-center relative font-Emilo font-bold`}
-
-        >
-          Life @ VIT-AP
-        </h1>
-        <p
-          className={`${styles.experience} w-[616px] h-[54px] text-center mt-3`}
-        >
-          Experience the vibrant heart of our campus life at Vitap University.
-        </p>
-        <button
-          className={`${styles.button} bg-[#5C0E14] items-center text-[#FDFEFF] w-[150px] h-[58px] rounded-full hover:bg-[#7A1820]`}
-        >
-          <Link href="/campuslife">
-            EXPLORE
-          </Link>
-        </button>
+          <div className={`${styles.textdiv} text-center`}>
+            <h4
+              className={`${styles.explore} text-[24px] text-[#5C0E14] font-Emilo font-bold`}
+            >
+              <i>Explore our campus</i>
+            </h4>
+            <h1
+              className={`${styles.life} text-[80px] text-center font-Emilo font-bold`}
+            >
+              Life @ VIT-AP
+            </h1>
+            <p className={`${styles.experience} w-[616px] h-[54px]`}>
+              Experience the vibrant heart of our campus life at Vitap
+              University.
+            </p>
+            <button
+              className={`${styles.button} bg-[#5C0E14] text-[#FDFEFF] w-[150px] h-[58px] rounded-full hover:bg-[#7A1820]`}
+            >
+              EXPLORE
+            </button>
+          </div>
+        </div>
       </div>
     </>
   );
