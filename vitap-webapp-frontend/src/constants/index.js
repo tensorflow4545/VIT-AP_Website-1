669b7c74bfb41225f7f7c4e0d318918cfc3cbd3a
kartik kumar <gtgkartik@gmail.com>
2024-01-03 12:54:36 +0530

    navbar changes

--- vitap-webapp-frontend/src/constants/index.js
+++ vitap-webapp-frontend/src/constants/index.js
@@ -182,7 +182,7 @@ export const announcements = [
     id: 1,
     content:"VITEEE 2024 - Apply Now",
     date:"1st Nov 2023",
-    link:"https://viteee.vit.ac.in/",
+    link:"/singleannouncement",
   },
   {
     id: 2,
