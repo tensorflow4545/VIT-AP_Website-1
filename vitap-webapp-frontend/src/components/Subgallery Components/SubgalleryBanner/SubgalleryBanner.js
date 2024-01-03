8bc14b80791a70e747ad44172cdb6a29159477d9
kartik kumar <gtgkartik@gmail.com>
2024-01-03 16:50:00 +0530

    Add Link component to image cards and update header styles

--- vitap-webapp-frontend/src/components/Subgallery Components/SubgalleryBanner/SubgalleryBanner.js
+++ vitap-webapp-frontend/src/components/Subgallery Components/SubgalleryBanner/SubgalleryBanner.js
@@ -2,37 +2,37 @@ import React from 'react'
 
 const SubgalleryBanner = () => {
     return (
-        <div className='max-h-[1600px] ls:min-h-[980px] w-full relative'>
-            <div className='w-full h-[338px] bg-[#E6CFCF] absolute top-0 left-0 -z-10 hidden ls:block'></div>
-            <div className='w-full h-[160px] bg-gradient-to-b from-[#E6CFCF] to-[#FDFEFF] absolute top-0 ls:top-[338px] left-0 -z-10'></div>
+        <div className='max-h-[1600px] lg:min-h-[980px] w-full relative'>
+            <div className='w-full h-[338px] bg-[#E6CFCF] absolute top-0 left-0 -z-10 hidden lg:block'></div>
+            <div className='w-full h-[160px] bg-gradient-to-b from-[#E6CFCF] to-[#FDFEFF] absolute top-0 lg:top-[338px] left-0 -z-10'></div>
             <div className='w-full h-[160px] bg-[#FDFEFF] absolute top-[500px] left-0 -z-10'></div>
             <div className='w-full h-[160px] bg-gradient-to-t from-[#E6CFCF] to-[#FDFEFF] absolute bottom-[160px] left-0 -z-10'></div>
             <div className='w-full h-[160px] bg-[#E6CFCF] absolute bottom-0 left-0 -z-10'></div>
-            <div className='mx-[20px] ls:mx-[50px] lg:mx-[100px] grid grid-cols-3 ls:grid-cols-6 gap-6 '>
-                <div className='bg-[#B78E8E] min-h-[100px] ls:min-h-[184px] max-w-[184px]'></div>
-                <div className='bg-[#B78E8E] min-h-[100px] ls:min-h-[184px] max-w-[184px]'></div>
-                <div className='bg-[#B78E8E] min-h-[100px] ls:min-h-[184px] max-w-[184px]'></div>
-                <div className='bg-[#B78E8E] min-h-[100px] ls:min-h-[184px] max-w-[184px]'></div>
-                <div className='bg-[#B78E8E] min-h-[100px] ls:min-h-[184px] max-w-[184px]'></div>
-                <div className='bg-[#B78E8E] min-h-[100px] ls:min-h-[184px] max-w-[184px]'></div>
-                <div className='bg-[#B78E8E] min-h-[100px] ls:min-h-[184px] max-w-[184px]'></div>
-                <div className='bg-[#B78E8E] min-h-[100px] ls:min-h-[184px] max-w-[184px]'></div>
-                <div className='bg-[#B78E8E] min-h-[100px] ls:min-h-[184px] max-w-[184px]'></div>
-                <div className='bg-[#B78E8E] min-h-[100px] ls:min-h-[184px] max-w-[184px]'></div>
-                <div className='bg-[#B78E8E] min-h-[100px] ls:min-h-[184px] max-w-[184px]'></div>
-                <div className='bg-[#B78E8E] min-h-[100px] ls:min-h-[184px] max-w-[184px]'></div>
-                <div className='bg-[#B78E8E] min-h-[100px] ls:min-h-[184px] max-w-[184px]'></div>
-                <div className='bg-[#B78E8E] min-h-[100px] ls:min-h-[184px] max-w-[184px]'></div>
-                <div className='bg-[#B78E8E] min-h-[100px] ls:min-h-[184px] max-w-[184px]'></div>
-                <div className='bg-[#B78E8E] min-h-[100px] ls:min-h-[184px] max-w-[184px]'></div>
-                <div className='bg-[#B78E8E] min-h-[100px] ls:min-h-[184px] max-w-[184px]'></div>
-                <div className='bg-[#B78E8E] min-h-[100px] ls:min-h-[184px] max-w-[184px]'></div>
-                <div className='bg-[#B78E8E] min-h-[100px] ls:min-h-[184px] max-w-[184px]'></div>
-                <div className='bg-[#B78E8E] min-h-[100px] ls:min-h-[184px] max-w-[184px]'></div>
-                <div className='bg-[#B78E8E] min-h-[100px] ls:min-h-[184px] max-w-[184px]'></div>
-                <div className='bg-[#B78E8E] min-h-[100px] ls:min-h-[184px] max-w-[184px]'></div>
-                <div className='bg-[#B78E8E] min-h-[100px] ls:min-h-[184px] max-w-[184px]'></div>
-                <div className='bg-[#B78E8E] min-h-[100px] ls:min-h-[184px] max-w-[184px]'></div>
+            <div className='mx-[20px] lg:mx-[50px] xl:mx-[100px] grid grid-cols-3 lg:grid-cols-6 gap-6 '>
+                <div className='bg-[#B78E8E] min-h-[100px] lg:min-h-[184px] max-w-[184px]'></div>
+                <div className='bg-[#B78E8E] min-h-[100px] lg:min-h-[184px] max-w-[184px]'></div>
+                <div className='bg-[#B78E8E] min-h-[100px] lg:min-h-[184px] max-w-[184px]'></div>
+                <div className='bg-[#B78E8E] min-h-[100px] lg:min-h-[184px] max-w-[184px]'></div>
+                <div className='bg-[#B78E8E] min-h-[100px] lg:min-h-[184px] max-w-[184px]'></div>
+                <div className='bg-[#B78E8E] min-h-[100px] lg:min-h-[184px] max-w-[184px]'></div>
+                <div className='bg-[#B78E8E] min-h-[100px] lg:min-h-[184px] max-w-[184px]'></div>
+                <div className='bg-[#B78E8E] min-h-[100px] lg:min-h-[184px] max-w-[184px]'></div>
+                <div className='bg-[#B78E8E] min-h-[100px] lg:min-h-[184px] max-w-[184px]'></div>
+                <div className='bg-[#B78E8E] min-h-[100px] lg:min-h-[184px] max-w-[184px]'></div>
+                <div className='bg-[#B78E8E] min-h-[100px] lg:min-h-[184px] max-w-[184px]'></div>
+                <div className='bg-[#B78E8E] min-h-[100px] lg:min-h-[184px] max-w-[184px]'></div>
+                <div className='bg-[#B78E8E] min-h-[100px] lg:min-h-[184px] max-w-[184px]'></div>
+                <div className='bg-[#B78E8E] min-h-[100px] lg:min-h-[184px] max-w-[184px]'></div>
+                <div className='bg-[#B78E8E] min-h-[100px] lg:min-h-[184px] max-w-[184px]'></div>
+                <div className='bg-[#B78E8E] min-h-[100px] lg:min-h-[184px] max-w-[184px]'></div>
+                <div className='bg-[#B78E8E] min-h-[100px] lg:min-h-[184px] max-w-[184px]'></div>
+                <div className='bg-[#B78E8E] min-h-[100px] lg:min-h-[184px] max-w-[184px]'></div>
+                <div className='bg-[#B78E8E] min-h-[100px] lg:min-h-[184px] max-w-[184px]'></div>
+                <div className='bg-[#B78E8E] min-h-[100px] lg:min-h-[184px] max-w-[184px]'></div>
+                <div className='bg-[#B78E8E] min-h-[100px] lg:min-h-[184px] max-w-[184px]'></div>
+                <div className='bg-[#B78E8E] min-h-[100px] lg:min-h-[184px] max-w-[184px]'></div>
+                <div className='bg-[#B78E8E] min-h-[100px] lg:min-h-[184px] max-w-[184px]'></div>
+                <div className='bg-[#B78E8E] min-h-[100px] lg:min-h-[184px] max-w-[184px]'></div>
             </div>
         </div>
     )
