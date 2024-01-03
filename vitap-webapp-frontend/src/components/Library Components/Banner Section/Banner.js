26834e3846e6f4fc60d350185e9983ab41c30881
allenki_akshay <allenkiakshay8322@gmail.com>
2024-01-03 15:52:59 +0530

    library page update

--- vitap-webapp-frontend/src/components/Library Components/Banner Section/Banner.js
+++ vitap-webapp-frontend/src/components/Library Components/Banner Section/Banner.js
@@ -4,7 +4,7 @@ import React from 'react'
 const LibBanner = () => {
     return (
         <>
-            <div className='max-w-[1560px] w-[100%] min-h-[600px] lx:min-h-[500px] h-[100%] hidden ls:block'>
+            <div className='max-w-[1560px] w-[100%] min-h-[600px] xl:min-h-[500px] h-[100%] hidden lg:block'>
                 <div className='relative'>
                     <div className='overflow-hidden'>
                         <svg xmlns="http://www.w3.org/2000/svg" width="1560" height="328" viewBox="0 0 1560 328" fill="none">
@@ -16,7 +16,7 @@ const LibBanner = () => {
 
                 </div> */}
 
-                    <div className='flex max-w-[1560px] w-[100%] min-h-[600px] lx:min-h-[500px] h-[100%] absolute top-0 left-0 px-[50px] md:px-[100px] py-[40px] gap-3 md:gap-5'>
+                    <div className='flex max-w-[1560px] w-[100%] min-h-[600px] xl:min-h-[500px] h-[100%] absolute top-0 left-0 px-[50px] md:px-[100px] py-[40px] gap-3 md:gap-5'>
                         <div className='flex-1 flex flex-col justify-center items-start'>
                             <h1 className='text-[46px] font-Emilio text-[#650010] font-bold'>About Library</h1>
                             <p className='text-[16px] font-Montserrat text-[#000]'>The VIT-AP University library a technology driven, user centric, innovative platform for the academic community which with the motto “Apply Knowledge and Improve life”. It maintains a perfect balance in providing priority to both traditional and modern concepts of library. The Library is well equipped with modem facilities and resources consisting of CDROMs, DVD-ROM, online databases, micro-documents, video cassettes, books, journals, etc. It has fully developed Digital Library equipped with necessary modern facilities in order to provide various digital mode Library services. It has rich collection of reference books, textbooks and research journals in electronic as well as in print form in the field of engineering, basic science and humanities, management, law and allied subjects. </p>
@@ -31,7 +31,7 @@ const LibBanner = () => {
                 </div>
             </div>
 
-            <div className='w-full max-h-[900px] bg-[#FDFEFF] block ls:hidden'>
+            <div className='w-full max-h-[900px] bg-[#FDFEFF] block lg:hidden'>
                 <div className='flex flex-col px-[30px] py-[20px] gap-3'>
                     <div className='flex flex-col gap-1'>
                         <h1 className='text-[24px] font-Emilio text-[#650010] font-bold'>About Library</h1>
--- vitap-webapp-frontend/src/components/Library Components/LibHeader Section/LibHeader.js
+++ vitap-webapp-frontend/src/components/Library Components/LibHeader Section/LibHeader.js
@@ -7,7 +7,7 @@ const LibHeader = () => {
             <div
                 style={{ backgroundImage: `url("/announcements.png")` }}
                 className="relative max-w-[1560px] w-full h-[300px] bg-center mx-auto flex items-center justify-center">
-                <h1 className="text-white text-[32px] ls:text-[45px] font-semibold text-center flex justify-center items-center">
+                <h1 className="text-white text-[32px] lg:text-[45px] font-semibold text-center flex justify-center items-center">
                     Library
                 </h1>
                 {/* <div className="curve3">
