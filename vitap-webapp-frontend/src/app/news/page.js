e3b10605f5e4f61bcc04fb78e424b0c1707547a4
Mohith705 <mohith.21bce7511@vitapstudent.ac.in>
2024-01-09 22:59:04 +0530

    working with news page

--- vitap-webapp-frontend/src/app/news/page.js
+++ vitap-webapp-frontend/src/app/news/page.js
@@ -2,13 +2,18 @@ import Footer from "@/components/HomePage Components/Footer Section/Footer";
 import NavbarBottom from "@/components/HomePage Components/Navbar/NavbarBottom";
 import NavbarUpper from "@/components/HomePage Components/Navbar/NavbarUpper";
 import AllNews from "@/components/NewsPage Components/AllNews";
+import NewsHeader from "@/components/NewsPage Components/NewsHeader";
+import NewsPage2 from "@/components/NewsPage Components/NewsPage";
 
 const NewsPage = () => {
     return ( 
         <>
             <NavbarUpper/>
             <NavbarBottom/>
-            <AllNews/>
+            {/* <AllNews/> */}
+            <NewsHeader />
+            {/* <NewsPage /> */}
+            <NewsPage2 />
             <Footer/>
         </>
      );
